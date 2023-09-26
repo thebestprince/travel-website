914886ecbd98e4eb25126178af1a6796105d0c5f
Dwayne Reed <dreedjr1992@gmail.com>
2023-09-25 23:50:30 -0500

    Click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import Travel_logo from '../images/Travel_logo.png';
 
 
 function Navbar() {
-  const [click, setClick] =useState(false);
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
     <nav className="navbar">
@@ -12,14 +14,15 @@ function Navbar() {
       <Link to='/' className='navbar-logo'>
      <img src={Travel_logo} alt="logo"/>
 
+
      </Link>
-     <div className='menu-icon'></div>
+     <div className='menu-icon' onClick={handleClick}>
      <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
     </div>
-
+    </div>
     </nav>
     </>
-  )
+  );
 }
 
 export default Navbar
